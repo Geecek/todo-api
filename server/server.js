1486b27220ce269ff8e4d608f688796f044eb64a
Geecek <gicalamaciej@gmail.com>
2018-09-03 12:24:15 +0200

    Add DELETE /todos/:id route

--- server/server.js
+++ server/server.js
@@ -41,7 +41,7 @@ app.get('/todos/:id', (req, res) => {
     if (!ObjectID.isValid(id)) {
         return res
             .status(404)
-            .send({})
+            .send()
     }
 
     Todo.findById(id).then((todo) => {
@@ -55,6 +55,34 @@ app.get('/todos/:id', (req, res) => {
         res
             .status(404)
             .send()
+    }).catch((err) => {
+        res
+            .status(400)
+            .send()
+    })
+})
+
+app.delete('/todos/:id', (req, res) => {
+    const id = req.params.id
+
+    if (!ObjectID.isValid(id)) {
+        return res
+            .status(404)
+            .send()
+    }
+
+    Todo.findByIdAndRemove(id).then((todo) => {
+        console.log(todo)
+        if (!todo) {
+            return res
+                .status(404)
+                .send()
+        }
+        res
+            .status(200)
+            .send({
+                todo
+            })
     }).catch((err) => {
         res
             .status(400)
